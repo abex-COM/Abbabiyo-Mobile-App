15e6633c79e2128da9d844c47f21f16f9f9bb09a
gt-yordanos <gt.yordanos@gmail.com>
2025-04-26 00:00:09 +0300

    fixed some minor issue

--- Frontend/baseUrl/baseUrl.jsx
+++ Frontend/baseUrl/baseUrl.jsx
@@ -1,3 +1,3 @@
 // src/api/config.js
-export default baseURL = "http://192.168.124.46:6000"; // 192.168.184.196  // 192.168.137.1
+export default baseURL = "http://192.168.98.129:6000"; // 192.168.184.196  // 192.168.137.1
 // https://abbabiyo-mobile-app.onrender.com
